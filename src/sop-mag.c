2afb21b6ae7ddc4f5e74d1e8ec9f9a12526d04e0
roleqzmn <maciej.majewski06@gmail.com>
2026-05-26 02:38:36 +0200

    stage1

--- src/sop-mag.c
+++ src/sop-mag.c
@@ -12,20 +12,23 @@ const char* spell_names[SPELL_TYPES] = {"Divination", "Summon Elemental", "Fireb
 #define MAX_CLIENTS 2
 #define MAX_NAME_LENGTH 14
 
-typedef struct __attribute__((__packed__)) packed
+typedef struct __attribute__((__packed__))
 {
-    char c1;
-    int i1;
-    char c2;
-    int i2;
-};
-typedef struct not_packed
+    int16_t spell;
+    int16_t x;
+    int16_t y;
+} cast_msg_t;
+
+typedef struct __attribute__((__packed__))
 {
-    char c1;
-    int i1;
-    char c2;
-    int i2;
-};
+    char type;
+    char padding;
+    union 
+    {
+        char name[MAX_NAME_LENGTH];
+        cast_msg_t cast;
+    }message;   
+} msg_t;
 
 void usage(char* name)
 {
@@ -36,6 +39,54 @@ void usage(char* name)
 
 int main(int argc, char** argv)
 {
-    printf("sizeof(struct packed) == %d\n", sizeof(struct packed));
-    printf("sizeof(struct not_packed) == %d\n", sizeof(struct not_packed));
+    if(argc!=2)
+    {
+         usage(argv[0]);
+    }
+    int port = atoi(argv[1]);
+    if(port<=0)
+    {
+        usage(argv[0]);
+    }
+
+    uint16_t serverfd = bind_inet_socket(port, SOCK_DGRAM, BACKLOG);
+    int count=0;
+    msg_t buffer;
+
+    while(count<4){
+        ssize_t bytes_read = recvfrom(serverfd, &buffer, sizeof(buffer), 0, NULL, NULL);
+        if(bytes_read<0)
+            ERR("recvfrom");
+        if(bytes_read!=sizeof(buffer))
+        {
+            fprintf(stderr, "Received message of invalid size\n");
+            continue;
+        }
+        if(buffer.type=='l'){
+            char name[15];
+            memcpy(name, buffer.message.name, MAX_NAME_LENGTH);
+            name[MAX_NAME_LENGTH]='\0';
+            printf("[Login] Welcome, %s!\n", name);
+            count++;
+        }
+        if(buffer.type == 'q'){
+            printf("[Quit] Someone quit. Goodbye!\n");
+            count++;
+        }
+        if(buffer.type == 'c'){
+            int16_t spell = ntohs(buffer.message.cast.spell);
+            int16_t x = ntohs(buffer.message.cast.x);
+            int16_t y = ntohs(buffer.message.cast.y);
+            if(spell<0 || spell>=SPELL_TYPES || x<0 || x>=BOARD_SIZE || y<0 || y>=BOARD_SIZE)
+            {
+                fprintf(stderr, "Received invalid cast message\n");
+                continue;
+            }
+            printf("[Cast] Someone casts %s onto %d,%d\n", spell_names[spell], x, y);
+        }
+        else{
+            continue;
+        }
+    }
+    close(serverfd);
 }
