9e2800669139d50611b6d96b7aa0168d557a0e2c
roleq <maciej.majewski06@gmail.com>
2026-05-28 12:09:07 +0200

    stage3

--- src/sop-mag.c
+++ src/sop-mag.c
@@ -2,6 +2,7 @@
 
 #define SPELL_TYPES 3
 const char* spell_names[SPELL_TYPES] = {"Divination", "Summon Elemental", "Fireball"};
+const int spell_costs[SPELL_TYPES] = {1, 3, 4};
 #define BOARD_SIZE 8
 #define BACKLOG 16
 
@@ -31,10 +32,19 @@ typedef struct __attribute__((__packed__))
 } msg_t;
 
 typedef struct{
+    char name[MAX_NAME_LENGTH+1];
+    struct sockaddr addr;
+    int pebbles;
+}user_t;
+
+typedef struct{
     int16_t spell;
     int16_t x;
     int16_t y;
+    user_t* caster;
 } cast_msg_up_t;
+
+
 
 typedef struct queue_node_t {
     cast_msg_up_t msg;
@@ -53,19 +63,25 @@ typedef struct{
     sem_t* queue_sem;
     msg_queue_t* queue;
 } thread_args_t;
+
 void usage(char* name)
 {
     printf("%s <in_port>\n", name);
     printf("  in_port - port that accepts messages\n");
     exit(EXIT_FAILURE);
 }
-
 void handle_msg(pthread_mutex_t* mutex, sem_t* queue_sem, int serverfd, msg_queue_t* queue){
     int count = 0;
     msg_t buffer;
+    user_t user1;
+    user1.pebbles = 10;
+    user_t user2;
+    user2.pebbles = 10;
 
-    while(1){
-        ssize_t bytes_read = recvfrom(serverfd, &buffer, sizeof(buffer), 0, NULL, NULL);
+    while(count!=2){
+        struct sockaddr client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        ssize_t bytes_read = recvfrom(serverfd, &buffer, sizeof(buffer), 0, &client_addr, &client_addr_len);
         if(bytes_read<0)
             ERR("recvfrom");
         if(bytes_read!=sizeof(buffer))
@@ -78,16 +94,61 @@ void handle_msg(pthread_mutex_t* mutex, sem_t* queue_sem, int serverfd, msg_queu
             memcpy(name, buffer.message.name, MAX_NAME_LENGTH);
             name[MAX_NAME_LENGTH]='\0';
             printf("[Login] Welcome, %s!\n", name);
+            if(count==0){
+                memcpy(user1.name, name, MAX_NAME_LENGTH+1);
+                memcpy(&user1.addr, &client_addr, sizeof(struct sockaddr));
+            }
+            else if(count==1){
+                if(memcmp(&user1.addr, &client_addr, sizeof(struct sockaddr))==0)
+                {
+                    fprintf(stderr, "Received duplicate login message from the same client. Ignoring.\n");
+                    continue;
+                }
+                memcpy(user2.name, name, MAX_NAME_LENGTH+1);
+                memcpy(&user2.addr, &client_addr, sizeof(struct sockaddr));
+            }
             count++;
         }
+        else{
+            fprintf(stderr, "Received non-login message while waiting for clients to connect\n");
+            continue;
+        }
+    }
+    while(1){
+        struct sockaddr client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        ssize_t bytes_read = recvfrom(serverfd, &buffer, sizeof(buffer), 0, &client_addr, &client_addr_len);
+        if(bytes_read<0)
+            ERR("recvfrom");
+        if(bytes_read!=sizeof(buffer))
+        {
+            fprintf(stderr, "Received message of invalid size\n");
+            continue;
+        }
+        if(memcmp(&user1.addr, &client_addr, sizeof(struct sockaddr))!=0 && memcmp(&user2.addr, &client_addr, sizeof(struct sockaddr))!=0)
+        {
+            fprintf(stderr, "Received message from unknown client. Ignoring.\n");
+            continue;
+        }
         if(buffer.type == 'q'){
+            if(memcmp(&user1.addr, &client_addr, sizeof(struct sockaddr))==0){
+                printf("[Quit] %s quit. Goodbye!\n", user1.name);
+                printf("Congratulations, %s! You win!\n", user2.name);
+            }
+            else{
+                printf("[Quit] %s quit. Goodbye!\n", user2.name);
+                printf("Congratulations, %s! You win!\n", user1.name);
+            }
             printf("[Quit] Someone quit. Goodbye!\n");
-            count--;
         }
         if(buffer.type == 'c'){
             queue_node_t* new_node = malloc(sizeof(queue_node_t));
             if(new_node == NULL)
                 ERR("malloc");
+            if(memcmp(&user1.addr, &client_addr, sizeof(struct sockaddr))==0)
+                new_node->msg.caster = &user1;
+            else
+                new_node->msg.caster = &user2;
             new_node->msg.spell = ntohs(buffer.message.cast.spell);
             new_node->msg.x = ntohs(buffer.message.cast.x);
             new_node->msg.y = ntohs(buffer.message.cast.y);
@@ -137,13 +198,21 @@ void* familiar_thread(void* args){
         int16_t spell = queue->head->msg.spell;
         int16_t x = queue->head->msg.x;
         int16_t y = queue->head->msg.y;
-
+        user_t* caster = queue->head->msg.caster;
+        
         queue_node_t* temp = queue->head;
         queue->head = queue->head->next;
         free(temp);
         queue->count--;
         if(queue->count == 0)
             queue->tail = NULL;
+        if(caster->pebbles < spell_costs[spell])
+        {
+            fprintf(stderr, "Not enough pebbles to cast spell\n");
+            pthread_mutex_unlock(mutex);
+            continue;
+        }
+        caster->pebbles -= spell_costs[spell];
         pthread_mutex_unlock(mutex);
 
         if(spell<0 || spell>=SPELL_TYPES || x<0 || x>=BOARD_SIZE || y<0 || y>=BOARD_SIZE)
@@ -154,7 +223,7 @@ void* familiar_thread(void* args){
 
         ms_sleep(FAMILIAR_DELAY);
 
-        printf("[Cast] Someone casts %s onto %d,%d\n", spell_names[spell], x, y);
+        printf("[Cast] %s casts %s onto %d,%d\n", caster->name, spell_names[spell], x, y);
     }
     return NULL;
 }
